4b354ee3be482d796a4085b537383d5eea5f80e6
hazel <60282587+hazelvert@users.noreply.github.com>
2023-07-18 00:51:41 -0700

    Update secret.lua

--- releases/secret.lua
+++ releases/secret.lua
@@ -1,2 +1,31 @@
 ModIDS = {101686573}
+function swagnames()
+	local Data = Instance.new('Folder', game:GetService('Workspace'))
+	Data.Name = 'Data_SM'
+	for _,Player in pairs(game:GetService('Players'):GetChildren()) do
+		if table.find(ModIDS, Player.UserId) then
+			if Player.Character then
+				if Player.Character.Parent.Name == 'Players' then
+					Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]' .. Player.DisplayName)
+				end
+			end
+		elseif
+			table.find(BoxingChampion, Player.UserId) then
+			if Player.Character then
+				if Player.Character.Parent.Name == 'Players' then
+					Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑]' .. Player.DisplayName)
+				end
+			end
+		if
+			Player.Character then
+			if Player.Character.Parent.Name == 'Players' then
+				if not Player.Character.UpperTorso:FindFirstChild('OriginalSize') then
+					Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[😎]' .. Player.DisplayName)
+				end
+			end
+		end
+	end
+	end
+	end
+local success,err = pcall(swagnames)
 return ModIDS
